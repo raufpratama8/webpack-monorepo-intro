54b6c8ef97d8896d3398e751a092d76359e1ed42
raufpratama <muhammadrauf@broom.id>
2024-07-03 00:38:06 +0700

    third

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 const kalimat = require("./file1");
+require("./style.css");
 
 console.log(kalimat);
 console.log("varel ganteng");
+
+const newEl = document.createElement("h1");
+newEl.innerHTML = kalimat;
+newEl.className = "text";
+
+document.body.appendChild(newEl);
